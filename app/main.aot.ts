7b908a4975ecdf33e2fc83342048b89d7680d89b
Govinda Alwani <govi2010@gmail.com>
2017-12-28 14:57:03 +0530

    Updating Webpack aot build for performance.

--- app/main.aot.ts
+++ app/main.aot.ts
@@ -1,6 +1,14 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScript } from "nativescript-angular/platform-static";
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/skip';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/debounce';
+import 'rxjs/add/operator/toPromise';
+
 // "./app.module.ngfactory" is a dynamically generated module when compiled with AoT.
 import { AppModuleNgFactory } from "./app.module.ngfactory";
 import { enableProdMode } from "@angular/core";
