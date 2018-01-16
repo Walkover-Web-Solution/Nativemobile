1fc7ea19d65023b69ef043f55ddff4dcb60e5074
Govinda Alwani <govi2010@gmail.com>
2018-01-16 14:31:04 +0530

    Govinda

--- app/settings/settings.component.ts
+++ app/settings/settings.component.ts
@@ -3,17 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ns-settings',
   moduleId: module.id,
-  templateUrl: `./settings.component.html`
+  templateUrl: './settings.component.html',
+  styleUrls: ["./settings.component.css"]
 })
 export class SettingsComponent {
   public items: Array<{ icon: string, text: string, path: string }>;
   constructor() {
     this.items = [
-      { text: 'Company Profile', icon: '', path: '' },
-      { text: 'Currencies', icon: '', path: '' },
-      { text: 'Taxes', icon: '', path: '' },
-      { text: 'Permission', icon: '', path: '' },
-      { text: 'Logout', icon: '', path: '' },
+      { text: 'Company Profile', icon: String.fromCharCode(0x61), path: '' },
+      { text: 'Currencies', icon: String.fromCharCode(0x61), path: '' },
+      { text: 'Taxes', icon: String.fromCharCode(0x62), path: '' },
+      { text: 'Permission', icon: String.fromCharCode(0x68), path: '' },
+      { text: 'Logout', icon: String.fromCharCode(0x67), path: '' },
     ]
   }
 }
