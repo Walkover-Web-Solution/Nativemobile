3010f168659cf4791432f5e1eb2bd65bd54103ec
Govinda Alwani <govi2010@gmail.com>
2018-01-31 17:41:39 +0530

    Vishal | add account navigate url

--- app/salesInvoice/component/stockAdd/stockAdd.component.ts
+++ app/salesInvoice/component/stockAdd/stockAdd.component.ts
@@ -6,6 +6,7 @@ import { AppState } from '~/store';
 import { Store } from '@ngrx/store';
 import { ObservableArray } from 'tns-core-modules/data/observable-array/observable-array';
 import { createSelector } from 'reselect';
+import { RouterExtensions } from 'nativescript-angular/router/router-extensions';
 
 @Component({
   selector: 'ns-stock-add',
@@ -15,7 +16,7 @@ import { createSelector } from 'reselect';
 })
 export class StockAddComponent implements OnInit {
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>, private _routerExtension: RouterExtensions) {
 
   }
 
