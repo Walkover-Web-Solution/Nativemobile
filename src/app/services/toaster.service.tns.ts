b218546d0c064dcd9f4a8e707581c6fca8b8a7cc
Govinda Alwani <govi2010@gmail.com>
2018-06-02 19:24:00 +0530

    Vishal | toaste service reimplemented

--- src/app/services/toaster.service.tns.ts
+++ src/app/services/toaster.service.tns.ts
@@ -1,40 +1,37 @@
 import {Injectable} from '@angular/core';
 import {APP_DEFAULT_TITLE} from '../app.constants';
-// import { ToastrService } from 'ngx-toastr';
-// import * as Toast from 'nativescript-toast';
-import {SnackBar} from 'nativescript-snackbar';
+import * as ToastModule from "nativescript-toast";
+import { Toast } from "nativescript-toast";
+// import {SnackBar} from 'nativescript-snackbar';
 import * as dialogs from 'ui/dialogs';
+import { isIOS } from "platform";
 
-let snackbar = new SnackBar();
+// let snackbar = new SnackBar();
 
 @Injectable()
 export class ToasterService {
-
+    private previousToast: Toast = null;
     constructor() {
 
     }
 
     public successToast(msg: string = 'Something went wrong', title: string = APP_DEFAULT_TITLE): void {
-        snackbar.simple(msg, 'black', 'green');
-        // let toast = Toast.makeText(msg);
-        // toast.show();
+        // snackbar.simple(msg, 'black', 'green');
+        this.show(msg);
     }
 
     public errorToast(msg: string = 'Something went wrong', title: string = APP_DEFAULT_TITLE): void {
-        snackbar.simple(msg, 'black', 'red');
-        // let toast = Toast.makeText(msg);
-        // toast.show();
+        // snackbar.simple(msg, 'black', 'red');
+        this.show(msg);
     }
 
     public warningToast(msg: string = 'Something went wrong', title: string = APP_DEFAULT_TITLE): void {
-        snackbar.simple(msg, 'black', 'orange');
-        // let toast = Toast.makeText(msg);
-        // toast.show();
+        // snackbar.simple(msg, 'black', 'orange');
+        this.show(msg);
     }
 
     public infoToast(msg: string = 'Something went wrong', title: string = APP_DEFAULT_TITLE): void {
-        // let toast = Toast.makeText(msg);
-        // toast.show();
+        this.show(msg);
     }
 
     public clearAllToaster(): void {
@@ -43,5 +40,21 @@ export class ToasterService {
 
     public confirm(obj: any) {
         return dialogs.confirm(obj);
+    }
+
+    private show(message: string, long?: boolean) {
+        if (this.previousToast) {
+            this.previousToast.cancel();
+            this.previousToast = null;
+        }
+
+        let t = ToastModule.makeText(message);
+
+        // if (!isIOS) {
+        //     (<any>t).getView().getChildAt(0).setTypeface(this.androidTypeface);
+        // }
+
+        t.show();
+        this.previousToast = t;
     }
 }
